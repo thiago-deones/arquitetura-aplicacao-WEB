f79dc8f5474c41be1ce4d37fabdc4715fc279c8b
Thiago <115191028+thiago-deones@users.noreply.github.com>
2025-10-06 20:06:16 -0300

    iniciando a prova e  fazendo alterações no venda mais

--- vendamais/src/main/java/br/com/empresa/vendamais/controller/ProdutoController.java
+++ vendamais/src/main/java/br/com/empresa/vendamais/controller/ProdutoController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +38,14 @@ public class ProdutoController {
         Produto produtoInserido = produtoService.insertNew(produto);
         return ResponseEntity.ok().body(produtoInserido);
     }
-
-
+    @PutMapping("/{id}")
+    public ResponseEntity<Produto> upDate(@PathVariable Long id, @RequestBody Produto produtoAlterado) {
+        Produto produto = produtoService.upDate(id, produtoAlterado);
+        return ResponseEntity.ok().body(produto);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> deletaById(@PathVariable Long id){
+        Boolean flag = produtoService.deleteById(id);
+        return ResponseEntity.ok().body(flag);
+    }
 }
