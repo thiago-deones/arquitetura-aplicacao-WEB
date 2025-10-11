09d943e9886038aff9a2fec30b0a31caecfcba7a
Thiago <115191028+thiago-deones@users.noreply.github.com>
2025-10-11 08:19:45 -0300

    Refactor code structure for improved readability and maintainability

--- departamento/src/main/java/com/empresa/departamento/controller/web/HomeController.java
+++ departamento/src/main/java/com/empresa/departamento/controller/web/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String redirecionarParaDepartamentos() {
+    public String HomeController() {
         return "index";
     }
 }
